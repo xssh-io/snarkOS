04a4b904f3b4af2417569a4ac3adc177a0ea0a8e
JakkuSakura <qjk2001@gmail.com>
2024-06-28 22:05:42 +0800

    feat: add script to start pool and worker

---
+++ run-prover-pool-worker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# USAGE examples:
+  # CLI with env vars: PROVER_PRIVATE_KEY=APrivateKey1...  ./run-prover.sh
+  # CLI with prompts for vars:  ./run-prover.sh
+
+# If the env var PROVER_PRIVATE_KEY is not set, prompt for it
+if [ -z "${PROVER_PRIVATE_KEY}" ]
+then
+  read -r -p "Enter the Aleo Prover account private key: "
+  PROVER_PRIVATE_KEY=$REPLY
+fi
+
+if [ "${PROVER_PRIVATE_KEY}" == "" ]
+then
+  echo "Missing account private key. (run 'snarkos account new' and try again)"
+  exit
+fi
+
+POOL_BASE_URL="http://localhost:3030"
+POOL_ADDRESS="0x"
+
+COMMAND="cargo run --release -- start --nodisplay --prover-pool-worker --private-key ${PROVER_PRIVATE_KEY} --pool-base-url ${POOL_BASE_URL} --pool-address ${POOL_ADDRESS}"
+
+for word in $*;
+do
+  COMMAND="${COMMAND} ${word}"
+done
+
+function exit_node()
+{
+    echo "Exiting..."
+    kill $!
+    exit
+}
+
+trap exit_node SIGINT
+
+echo "Running an Aleo Prover node..."
+$COMMAND &
+
+while :
+do
+  echo "Checking for updates..."
+  git stash
+  STATUS=$(git pull)
+
+  if [ "$STATUS" != "Already up to date." ]; then
+    echo "Updated code found, rebuilding and relaunching prover"
+    cargo clean
+    kill -INT $!; sleep 2; $COMMAND &
+  fi
+
+  sleep 1800;
+done
