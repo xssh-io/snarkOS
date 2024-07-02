59f05d61844c451a880d16e80e654d6954e32f7b
L-Ryland <saintleso@gmail.com>
2024-07-02 22:02:46 +0800

    update

--- node/src/pool/export.rs
+++ node/src/pool/export.rs
@@ -9,12 +9,12 @@ use crate::handle::SubmitSolutionRequest;
 
 #[async_trait::async_trait]
 pub trait ExportSolution: Send + Sync {
-    fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()>;
+    async fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()>;
 }
 
 #[async_trait::async_trait]
 impl ExportSolution for () {
-    fn export_solution(&self, _solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
+    async fn export_solution(&self, _solution: &SubmitSolutionRequest, _: SocketAddr) -> Result<()> {
         Ok(())
     }
 }
@@ -27,7 +27,11 @@ impl<N: Network> ExportSolutionClickhouse<N> {
     pub fn new(client: ClientHandle) -> Self {
         Self { client, network: None }
     }
-    pub async fn export_solution(&mut self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
+}
+
+#[async_trait::async_trait]
+impl<N: Network> ExportSolution for ExportSolutionClickhouse<N> {
+    async fn export_solution(&mut self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
         let submitter_address = solution.address.clone();
         let partial = solution.solution.partial_solution.clone();
         let now = Local::now();
@@ -43,17 +47,6 @@ impl<N: Network> ExportSolutionClickhouse<N> {
             solution.solution.target,
         );
         self.client.execute(query).await?;
-        Ok(())
-        // let new_sol = Solution::new(partial_solution, target)
-    }
-}
-
-#[async_trait::async_trait]
-impl<N: Network> ExportSolution for ExportSolutionClickhouse<N> {
-    fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
-        for export in self {
-            export.export_solution(solution, ip_addr)?;
-        }
         Ok(())
     }
 }
