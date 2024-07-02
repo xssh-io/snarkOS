547ea2e94c2e8e5c367c849d65e068ac9f28cbd8
L-Ryland <saintleso@gmail.com>
2024-07-02 19:05:48 +0800

    feat: implement export

--- node/src/pool/export.rs
+++ node/src/pool/export.rs
@@ -1,47 +1,56 @@
+use std::net::SocketAddr;
+
 use anyhow::Result;
+use chrono::Local;
+use snarkvm::prelude::Network;
 
 use crate::handle::SubmitSolutionRequest;
 
 #[async_trait::async_trait]
 pub trait ExportSolution: Send + Sync {
-    fn export_solution(&self, solution: &SubmitSolutionRequest) -> Result<()>;
+    fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()>;
 }
 
 #[async_trait::async_trait]
 impl ExportSolution for () {
-    fn export_solution(&self, _solution: &SubmitSolutionRequest) -> Result<()> {
+    fn export_solution(&self, _solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
         Ok(())
     }
 }
 
-pub struct ExportSolutionClickhouse {
+pub struct ExportSolutionClickhouse<N: Network> {
     client: clickhouse_rs::Client,
 }
-impl ExportSolutionClickhouse {
+impl<N: Network> ExportSolutionClickhouse<N> {
     pub fn new(client: clickhouse_rs::Client) -> Self {
         Self { client }
     }
-    pub async fn export_solution(&self, solution: &SubmitSolutionRequest) -> Result<()> {
+    pub async fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
+        let submitter_address = solution.address;
+        let partial = solution.solution.partial_solution;
+        let now = Local::now();
         let query = format!(
-            "INSERT INTO solutions (id, problem_id, user_id, language, code, created_at, updated_at) VALUES ({}, {}, {}, {}, {}, {}, {})",
-            solution.id,
-            solution.problem_id,
-            solution.user_id,
-            solution.language,
-            solution.code,
-            solution.created_at,
-            solution.updated_at,
+            "INSERT INTO solution (datetime, submitter_address, submitter_ip, solution_id, epoch_hash, address, counter, target) VALUES ({}, {}, {}, {}, {}, {}, {}, {})",
+            now,
+            submitter_address,
+            ip_addr,
+            partial.solution_id,
+            partial.epoch_hash,
+            partial.address,
+            partial.counter,
+            solution.solution.target,
         );
         self.client.execute(query).await?;
         Ok(())
+        // let new_sol = Solution::new(partial_solution, target)
     }
 }
 
 #[async_trait::async_trait]
-impl ExportSolution for ExportSolutionClickhouse {
-    fn export_solution(&self, solution: &SubmitSolutionRequest) -> Result<()> {
+impl<N: Network> ExportSolution for ExportSolutionClickhouse<N> {
+    fn export_solution(&self, solution: &SubmitSolutionRequest, ip_addr: SocketAddr) -> Result<()> {
         for export in self {
-            export.export_solution(solution)?;
+            export.export_solution(solution, ip_addr)?;
         }
         Ok(())
     }
