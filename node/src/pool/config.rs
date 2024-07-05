903bdc7fcda452c3907d002a8951bbe2808c3c33
L-Ryland <saintleso@gmail.com>
2024-07-05 15:12:50 +0800

    fix; concat clickhouse url

--- node/src/pool/config.rs
+++ node/src/pool/config.rs
@@ -11,14 +11,21 @@ pub struct PoolConfig {
     pub private_key: String,
     base_url: String,
     pub clickhouse_password: Option<String>,
+    clickhouse_host: Option<String>,
+    clickhouse_port: String,
 }
 impl PoolConfig {
     pub fn base_url(&self) -> Url {
         self.base_url.parse().with_context(|| format!("Invalid base URL: {}", self.base_url)).unwrap()
     }
     pub async fn get_export<N: Network>(&self) -> Result<Arc<dyn ExportSolution>> {
+        let host = match &self.clickhouse_host {
+            Some(host) => host,
+            None => "localhost",
+        };
+        let port = &self.clickhouse_port;
         if let Some(password) = &self.clickhouse_password {
-            let url = format!("tcp://default:{}@host:9000/clicks?compression=lz4&ping_timeout=42ms", password);
+            let url = format!("tcp://default:{}@{}:{}/clicks?compression=lz4&ping_timeout=42ms", password, host, port);
 
             let clickhouse = clickhouse_rs::Pool::new(url);
             let handle = clickhouse.get_handle().await?;
